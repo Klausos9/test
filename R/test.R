d789c83722259b70f987dfc8676ae3975050df37
Liana <liana.napalkova@gmail.com>
2015-06-26 21:58:13 +0200

    update

--- R/test.R
+++ R/test.R
@@ -10,3 +10,18 @@ test <- function(input){
         newdata$tv <- sqrt(newdata$x)
         return(newdata)
 }
+
+test2 <- function (input) 
+{
+        newdata <- if (is.character(input) && file.exists(input)) {
+                read.csv(input)
+        }
+        else {
+                as.data.frame(input)
+        }
+        stopifnot("age" %in% names(newdata))
+        stopifnot("marital" %in% names(newdata))
+        newdata$age <- as.numeric(newdata$age)
+        newdata$tv <- as.vector(predict.gam(tv_model, newdata = newdata))
+        return(newdata)
+}
