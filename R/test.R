9e584ddd00f5306ae909131266e5db83a93edb68
Liana <liana.napalkova@gmail.com>
2015-06-26 21:23:00 +0200

    Update

--- R/test.R
+++ R/test.R
@@ -1,3 +1,12 @@
-test <- function(x){
-        sqrt(x)
+test <- function(input){
+        newdata <- if (is.character(input) && file.exists(input)) {
+                read.csv(input)
+        }
+        else {
+                as.data.frame(input)
+        }
+        stopifnot("x" %in% names(newdata))
+        newdata$x <- as.numeric(newdata$age)
+        newdata$tv <- sqrt(newdata$x)
+        return(newdata)
 }
