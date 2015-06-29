a383efdcdae192999cdc8ced6c0a913f23c8a9df
Liana <liana.napalkova@gmail.com>
2015-06-29 08:29:06 +0200

    sqrt(x)

--- R/test.R
+++ R/test.R
@@ -1,6 +1,3 @@
 test <- function(x=""){
-        if (x == "") {
-                stop("Tell me x value!")
-        }
-        list(message = paste("hello", x, "! This is", R.Version()$version.string))
+    sqrt(x)
 }
